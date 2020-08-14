7818e77c392125d2e45a468936e798ca4068dfc5
Sukhesh Nuthalapati <sukheshnuthalapati@Sukheshs-MacBook-Pro.local>
2020-08-14 16:53:59 -0400

    placeholder login page and mapview page

--- GarudaAndroid/app/src/main/java/com/garudahacks/MainActivity.java
+++ GarudaAndroid/app/src/main/java/com/garudahacks/MainActivity.java
@@ -2,12 +2,29 @@ package com.garudahacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.mapButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent map = new Intent(v.getContext(), MapActivity.class);
+                startActivity(map);
+            }
+        });
+
+
+
     }
 }
